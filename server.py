64778462d53c1c922c2a7777dc31415454508691
scriscuoli-aic <scriscuoli@gmail.com>
2026-06-06 21:50:59 -0400

    drag and drop works

--- server.py
+++ server.py
@@ -1,13 +1,33 @@
 from flask import Flask,render_template, redirect, request,session
 from flask_wtf import FlaskForm,CSRFProtect
 from wtforms import StringField, PasswordField, SubmitField, FileField
-from flask_wtf.file import FileAllowed,FileRequired
+from flask_wtf.csrf import CSRFProtect, CSRFError
 from werkzeug.utils import secure_filename
 from waitress import serve
+from auth.auth import dbname
+from flask_dropzone import Dropzone
 
 import os
 
-from auth.auth import dbname
+
+csrf = CSRFProtect()
+dropzone = Dropzone()
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SDC'
+app.config['APP_NAME'] = dbname
+app.config["SESSION_PERMANENT"] = False     # Sessions expire when the browser is closed
+app.config["SESSION_TYPE"] = "filesystem"     # Store session data in files
+app.config['WTF_CSRF_HEADERS'] = ['X-CSRF-Token', 'X-CSRFToken']
+app.config['DROPZONE_ENABLE_CSRF'] = True
+app.config['DROPZONE_MAX_FILE_SIZE'] = 50  # MB
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # 50MB in bytes
+
+
+csrf.init_app(app)
+dropzone.init_app(app)
+
 from about.about import about_bp
 from admin.admin import admin_bp
 from documents.documents import documents_bp
@@ -16,11 +36,6 @@ from auth.auth import  auth_bp
 from upload.upload import upload_bp
 from user.user import user_bp
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SDC'
-app.config['APP_NAME'] = dbname
-app.config["SESSION_PERMANENT"] = False     # Sessions expire when the browser is closed
-app.config["SESSION_TYPE"] = "filesystem"     # Store session data in files
 
 # blueprints
 app.register_blueprint(about_bp,url_prefix='/DocsApp/about')
@@ -31,7 +46,7 @@ app.register_blueprint(auth_bp,url_prefix='/DocsApp')
 app.register_blueprint(upload_bp,url_prefix='/DocsApp/upload')
 app.register_blueprint(user_bp,url_prefix="/DocsApp/user")
 
-csrf = CSRFProtect(app)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -40,6 +55,10 @@ def page_not_found(error):
 @app.errorhandler(500)
 def you_broke_it(errror):
     return "you_broke_it...",500
+
+@app.errorhandler(CSRFError)
+def csrf_error(e):
+    return e.description, 400
 
 mode = "dev"
 
--- upload/upload.py
+++ upload/upload.py
@@ -1,5 +1,7 @@
-from flask import Blueprint,render_template,redirect,session
+from flask import Blueprint,render_template,redirect,session,request
 import util
+import os
+
 
 upload_bp = Blueprint('upload_bp', __name__,
                      template_folder='templates',
@@ -18,3 +20,20 @@ def show_upload():
         "pageDescription": ""
     }
     return render_template('upload/upload.html',tvals=tvals)
+
+@upload_bp.route('/handler', methods=['POST'])
+def show_upload_handler():
+    if not session.get("name"):
+        return redirect("/DocsApp/login")
+    tvals = {
+        "site": util.getSiteName(),
+        "database" : util.dbname,
+        "name": session.get("name"),
+        "title":"Upload Handler",
+        "pageTitle": "",
+        "pageDescription": ""
+    }
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join('test-data/drop', f.filename))
+    return render_template('upload/upload_handler.html',tvals=tvals)
