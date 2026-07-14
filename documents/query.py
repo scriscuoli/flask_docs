7093147666848d5b22048266fc5cc8521e92d602
scriscuoli-aic <scriscuoli@gmail.com>
2026-07-14 17:04:58 -0400

    merge docs

--- documents/query.py
+++ documents/query.py
@@ -5,16 +5,28 @@ from datetime import date, timedelta
 
 dbname = util.dbname
 
-def create_document(dc_name:str, dc_date:str):
+def get_specific_document(dc_name:str, dc_date:str):
     dc_id = 0
     connection = util.db_connect(dbname)
     cursor = connection.cursor(dictionary=True)
-    sqlString = f"insert into documents (dc_name,dc_date) values ('{dc_name}','{dc_date}');"
-    #print(sqlString)
-    
+    sqlString = f"select dc_id from documents where dc_name = '{dc_name}' and dc_date = '{dc_date}';"
     cursor.execute(sqlString)
-    dc_id = cursor.lastrowid
-    connection.commit()
+    full_set = cursor.fetchall()
+    for row in full_set:
+        dc_id = row['dc_name']
+    return dc_id
+
+def create_document(dc_name:str, dc_date:str):
+    dc_id = get_specific_document(dc_name,dc_date)
+    if dc_id == 0:
+        connection = util.db_connect(dbname)
+        cursor = connection.cursor(dictionary=True)
+        sqlString = f"insert into documents (dc_name,dc_date) values ('{dc_name}','{dc_date}');"
+        #print(sqlString)
+        
+        cursor.execute(sqlString)
+        dc_id = cursor.lastrowid
+        connection.commit()
     return dc_id
 
 def update_document(dc_id,dc_name,dc_date):
